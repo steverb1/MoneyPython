5fb7970dd6f9ff90d11f496014efcdef6aa7652f
steve.bement <steve.bement@accenture.com>
2024-09-06 14:01:14 -0600

    extracted class under test to class attribute

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,20 +1,22 @@
+import pytest
+
 from money.money import Money
 from money.money_adder import MoneyAdder
 
 
 class TestMoney:
+    adder = MoneyAdder()
+
     def test_2and4_equal6(self):
-        adder = MoneyAdder()
         money1 = Money(4)
         money2 = Money(2)
 
-        money_sum = adder.add(money1, money2)
+        money_sum = self.adder.add(money1, money2)
         assert money_sum.amount == 6
 
     def test_3and5_equal8(self):
-        adder = MoneyAdder()
         money1 = Money(3)
         money2 = Money(5)
 
-        money_sum = adder.add(money1, money2)
+        money_sum = self.adder.add(money1, money2)
         assert money_sum.amount == 8
